244b9e796dadb2812211b91f9e390598a4105a11
cesarjimenezpy <cesarjimenezinv.94@gmail.com>
2025-02-15 11:48:20 -0300

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,6 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
     #SQLALCHEMY_DATABASE_URI = 'sqlite:///stock.db'
+    #solo para redeployar
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.klfeqyqhpglowmigmmpw:8lnNmvbiaCNfey0i@aws-0-sa-east-1.pooler.supabase.com:6543/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
