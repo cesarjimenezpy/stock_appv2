b71aaa03e76475b3de45ff706fe674b5f31f0765
cesarjimenezpy <cesarjimenezinv.94@gmail.com>
2025-02-15 11:29:42 -0300

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,5 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
     #SQLALCHEMY_DATABASE_URI = 'sqlite:///stock.db'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:8lnNmvbiaCNfey0i@db.klfeqyqhpglowmigmmpw.supabase.co:5432/postgres'
-    #postgresql://postgres:[YOUR-PASSWORD]@db.klfeqyqhpglowmigmmpw.supabase.co:5432/postgres
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.klfeqyqhpglowmigmmpw:8lnNmvbiaCNfey0i@aws-0-sa-east-1.pooler.supabase.com:6543/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
