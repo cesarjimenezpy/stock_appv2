a5b6aa16fc8312b02a5636df835c5e8ad6698132
cesarjimenezpy <cesarjimenezinv.94@gmail.com>
2025-05-06 17:16:09 -0400

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
     #SQLALCHEMY_DATABASE_URI = 'sqlite:///stock.db'
     #solo para redeployar
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.klfeqyqhpglowmigmmpw:8lnNmvbiaCNfey0i@aws-0-sa-east-1.pooler.supabase.com:6543/postgres'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://neondb_owner:npg_g7J9PuqMaEYs@ep-noisy-snow-acje39qt-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
